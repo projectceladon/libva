f91b60d06e3c5aa86be4c020e966449c42e8d54b
Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
2018-09-12 13:13:22 +0800

    Fix compilation warning (uninit and wrong variable types) for Android O MR1
    Change-Id: I21b784d572a285121c3dae81a585310b1db79305
    Signed-off-by: Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>

--- va/va.c
+++ va/va.c
@@ -410,7 +410,7 @@ static VAStatus va_openDriver(VADisplay dpy, char *driver_name)
                 { VA_MAJOR_VERSION, 2 },
                 { VA_MAJOR_VERSION, 1 },
                 { VA_MAJOR_VERSION, 0 },
-                { -1, }
+                { -1, -1}
             };
 
             for (i = 0; compatible_versions[i].major >= 0; i++) {
@@ -947,7 +947,7 @@ va_impl_query_surface_attributes(
         { VASurfaceAttribMinHeight,     VAGenericValueTypeInteger },
         { VASurfaceAttribMaxHeight,     VAGenericValueTypeInteger },
         { VASurfaceAttribMemoryType,    VAGenericValueTypeInteger },
-        { VASurfaceAttribNone, }
+        { VASurfaceAttribNone,          VAGenericValueTypeInteger }
     };
 
     if (!out_attribs || !out_num_attribs_ptr)
