ecb16b1b4abc181f53ab07f93db6249f52ccbed5
Shaofeng Tang <shaofeng.tang@intel.com>
2021-07-21 15:36:56 +0800

    Update the driver path
    For MediaSDK 2021Q1 release upgraded
    
    Tracked-On: OAM-98044
    Signed-off-by: Shaofeng Tang <shaofeng.tang@intel.com>

--- va/Android.mk
+++ va/Android.mk
@@ -25,8 +25,8 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-LIBVA_DRIVERS_PATH_32 := /vendor/lib/:/system/lib
-LIBVA_DRIVERS_PATH_64 := /vendor/lib64/:/system/lib64
+LIBVA_DRIVERS_PATH_32 := /vendor/lib
+LIBVA_DRIVERS_PATH_64 := /vendor/lib64
 
 include $(CLEAR_VARS)
 
